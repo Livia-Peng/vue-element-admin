9c710d9c841e608bd2b1153961528aeb3f1b61cb
Livia-Peng <livia_peng@163.com>
2024-11-28 02:11:45 +0800

    svg 图片导入与注册；登录页样式

--- src/main.js
+++ src/main.js
@@ -2,5 +2,13 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import installElementPlus from './plugins/element'
+// import * as icons from '@element-plus/icons'  // element icon 全局导入
+import './styles/index.scss' // 全局样式
+import installIcons from './icons'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+// Object.keys(icons).forEach((k) => app.component(k, icons[k]))
+installElementPlus(app)
+installIcons(app)
+app.use(store).use(router).mount('#app')
